1cc996f5f0c3d908ff70b4f2e1074ffb5e907470
BLOCKSTORM_ <36893722+BLOCKSTORMPE@users.noreply.github.com>
2018-02-27 18:13:46 +0100

    Update Main.php

--- src/test/Main.php
+++ src/test/Main.php
@@ -1,1 +1,34 @@
 
+<?php
+namespace test;
+#PocketMine-Based imports
+use pocketmine\plugin\PluginBase;
+use pocketmine\{Server, Player};
+use pocketmine\utils\{Config, TextFormat};
+#class declaration(Name should be filename)
+class Main extends PluginBase{
+	#Function when the plugin loads the first time
+	public function onEnable(){
+if(!is_dir($this->getDataFolder())){
+
+mkdir($this->getDataFolder());
+
+}
+
+
+		#New Config created for the startup message
+		$config = new Config($this->getDataFolder()."config.yml", Config::YAML);
+			#Setting a string to the yaml file
+			$config->set("Startup-Message", "TestPlugin was started");
+			#saving the yaml file
+			$config->save();
+			$this->getServer()->getLogger()->info("Initial Basic config successfully created!");
+	}
+	#message every time the server starts
+	public function onLoad(){
+		$cfg = new Config($this->getDataFolder()."config.yml", Config::YAML);
+		#sending message to console
+		$this->getServer()->getLogger()->info(TextFormat::RED . $cfg->get("Startup-Message"));
+		}
+	}
+		
